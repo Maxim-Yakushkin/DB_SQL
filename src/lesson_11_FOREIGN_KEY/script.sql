93c71c7e02b681e164650b8e9b7f10c88845e6bc
Maxim <max-yakushkin1@yandex.ru>
2021-02-09 23:26:02 +0300

    Foreign key

---
+++ src/lesson_11_FOREIGN_KEY/script.sql
@@ -0,0 +1,31 @@
+CREATE TABLE company_storage.company
+(
+    id   BIGSERIAL PRIMARY KEY,
+    name VARCHAR(128) UNIQUE NOT NULL,
+    date DATE UNIQUE         NOT NULL CHECK ( date > '1995-01-01' AND date < '2020-01-01')
+);
+
+CREATE TABLE company_storage.employee
+(
+    id         BIGSERIAL PRIMARY KEY,
+    first_name VARCHAR(128) NOT NULL,
+    last_name  VARCHAR(128) NOT NULL,
+    salary     MONEY,
+    company_id BIGINT REFERENCES company_storage.company (id)
+--     FOREIGN KEY (company_id) REFERENCES company_storage.company (id)
+);
+
+DROP TABLE company_storage.employee;
+
+INSERT INTO company_storage.employee(first_name, last_name, salary, company_id)
+VALUES ('Ivan', 'Ivanov', 1000, 1),
+       ('Petr', 'Petrov', 2000, 2),
+       ('Arny', 'Poramonov', NULL, 3),
+       ('Petr', 'Sidorov', 2100, 4),
+       ('Sveta', 'Svetikova', 1500, NULL);
+
+SELECT *
+FROM company_storage.company;
+
+SELECT *
+FROM company_storage.employee;
